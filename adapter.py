4febbbb8ab4139e48ce3740ae719f38bfb81e0d4
angel012912 <angel_012912@hotmail.com>
2023-04-11 18:05:09 -0600

    Adapter defined

--- adapter.py
+++ adapter.py
@@ -1,4 +1,8 @@
-# Task: Write an adapter for the Speedometer to make it work with the CarDisplay
+# Author: Jose Angel Garcia Gomez
+# Date: 11-04-2023
+# Task: Write an adapter for the Speedometer to make it work
+# with the CarDisplay
+
 import random
 
 
@@ -16,13 +20,25 @@ class MphSpeedometer:
 
 
 class MphToKphSpeedometerAdapter:
-    raise NotImplementedError('Implement me!')
+    """ TODO: Implement the adapter here.
+    It should take in the MphSpeedometer """
+
+    _KPH_MPH_CONVERSION_RATIO = 1.60934
+
+    def __init__(self, speedometer: MphSpeedometer):
+        self.speedometer = speedometer
+
+    def get_speed(self):
+        """Returns speed in KPH"""
+        speed = self.speedometer.get_speed()
+        return speed * self._KPH_MPH_CONVERSION_RATIO
 
 
 class CarDisplay:
-    """TODO: change to take in the MphtoKphSpeedometerAdapter instead of the Speedometer"""
+    """TODO: change to take in the MphtoKphSpeedometerAdapter instead
+    of the Speedometer"""
 
-    def __init__(self, speedometer: MphSpeedometer):
+    def __init__(self, speedometer: MphToKphSpeedometerAdapter):
         self.speedometer = speedometer
 
     def show_speed(self):
@@ -32,6 +48,7 @@ class CarDisplay:
 
 if __name__ == '__main__':
     """TODO: change as needed for your new adapter"""
-    speedometer = MphSpeedometer()
-    car_display = CarDisplay(speedometer)
+    mph_speedometer = MphSpeedometer()
+    adapter = MphToKphSpeedometerAdapter(mph_speedometer)
+    car_display = CarDisplay(adapter)
     car_display.show_speed()
